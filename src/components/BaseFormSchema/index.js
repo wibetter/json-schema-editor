923401f8ec11310aa197b62094abb71723254925
liudan185 <liudan185@jd.com>
2020-06-19 10:47:17 +0800

    feat(新版JSONSchema)：增加readOnly状态

--- src/components/BaseFormSchema/index.js
+++ src/components/BaseFormSchema/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { Input, Select, Tooltip } from 'antd';
 const { Option } = Select;
 import { TypeList } from '$data/TypeList';
-import { PlusOutlined } from '@ant-design/icons';
+import { PlusOutlined, CloseOutlined } from '@ant-design/icons';
 import './index.scss';
 
 class BaseFormSchema extends React.PureComponent {
@@ -31,18 +31,21 @@ class BaseFormSchema extends React.PureComponent {
   render() {
     const { parentType, jsonKey, targetJsonData } = this.props;
 
+    const readOnly = targetJsonData.readOnly || false; // 是否不可编辑状态，默认为可编辑状态
+
     const currentTypeList = this.getCurrentTypeList(parentType); // 根据父级元素类型获取可供使用的类型清单
 
     return (
       <div className="base-schema-box">
         <div className="key-input-item">
-          <Input value={jsonKey} />
+          <Input value={jsonKey} disabled={readOnly} />
         </div>
         <div className="type-select-item">
           <Select
             defaultValue={targetJsonData.format}
             style={{ width: 120 }}
             onChange={this.selectHandleChange}
+            disabled={readOnly}
           >
             {currentTypeList.map((item) => {
               return (
@@ -54,9 +57,12 @@ class BaseFormSchema extends React.PureComponent {
           </Select>
         </div>
         <div className="title-input-item">
-          <Input value={targetJsonData.title} />
+          <Input value={targetJsonData.title} disabled={readOnly} />
         </div>
         <div className="operate-item">
+          {!readOnly && (
+            <CloseOutlined className="operate-btn delete-operate" />
+          )}
           <PlusOutlined className="operate-btn" />
         </div>
       </div>
